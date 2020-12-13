3d3bc13813f97597f86b29faf4485389ea001f68
Adam Hayward <a.hayward11@gmail.com>
2020-12-12 23:58:02 -0900

    bugs resolved

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+// const created to references external files & modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+/* const created to define the name of the produced file and
+the path to where the file will be written*/
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+//const to render functions defined inside seperate constructor classes
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
-
-let allEmployees = [];
-
+// varrible to store each indvidule inside an arrey that will print to the 'team.html' document
+var allEmployees = [];
+// function to prompt user is created 
 const promptUser = () =>
     inquirer.prompt([
         {
@@ -61,30 +63,32 @@ const promptUser = () =>
         {
             type: 'confirm',
             name: 'additionalEmployee',
-            message: 'Will you be adding additional employees at this time?',
+            message: 'Will you be adding additional employees?',
         }
     ])
         .then((answers) => {
-            // console.log(JSON.stringify(answers, null, ' '));
+            // switch created to replace questions based on selected emplyee role
             switch (answers.role) {
                 case 'Manager':
                     const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
                     allEmployees.push(manager);
-                    console.log(allEmployees);
+                    console.log('Employee added successfully');
                     break;
                 case 'Engineer':
                     const engineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
                     allEmployees.push(engineer);
-                    console.log(allEmployees);
+                    console.log('Employee added successfully');
                     break;
                 case 'Intern':
                     const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
                     allEmployees.push(intern);
-                    console.log(allEmployees);
+                    console.log('Employee added successfully');
                     break;
             }
+            // allows for addiditional prompts for a new employee if user selects yes
             if (answers.additionalEmployee) {
                 promptUser();
+            // if user selects no then team.html will be created with the user's data
             } else {
                 const renderHTML = render(allEmployees);
                 fs.writeFile(outputPath, renderHTML, (err) => {
@@ -96,29 +100,6 @@ const promptUser = () =>
                 })
             }
         })
-
+// funtion initiated when app is ran the command line (node app.js)
 promptUser();
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
